94177f815b885ed2ee9637e74cfb23bc51f4e207
simondunvall <simon.dunvall@iths.se>
2023-01-07 21:28:28 +0100

    place building done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,20 @@ public class GameManager : MonoBehaviour
     public Tile[] tiles;
 
     private void Update()
+    {
+        PlaceBuilding();
+    }
+
+    public void BuyBuilding(Building building)
+    {
+        customCursor.gameObject.SetActive(true);
+        customCursor.GetComponent<SpriteRenderer>().sprite = building.GetComponent<SpriteRenderer>().sprite;
+        Cursor.visible = false;
+
+        buildingToPlace = building;
+    }
+
+    public void PlaceBuilding()
     {
         if (Input.GetMouseButton(0) && buildingToPlace != null)
         {
@@ -26,10 +41,17 @@ public class GameManager : MonoBehaviour
                     nearestTile = tile;
                 }
             }
-            if (nearestTile.isOccupied == false)
+
+            List<Tile> allNeighbouringTiles = FindAllTileNeighbors(nearestTile.transform.position);
+            if (nearestTile.isOccupied == false && allNeighbouringTiles.Count == 8 && allNeighbouringTiles.All(tile => tile.isOccupied == false))
             {
                 Instantiate(buildingToPlace, nearestTile.transform.position, Quaternion.identity);
                 buildingToPlace = null;
+
+                foreach (Tile tile in allNeighbouringTiles)
+                {
+                    tile.isOccupied = true;
+                }
                 nearestTile.isOccupied = true;
                 customCursor.gameObject.SetActive(false);
                 Cursor.visible = true;
@@ -37,12 +59,32 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void BuyBuilding(Building building)
-    {
-        customCursor.gameObject.SetActive(true);
-        customCursor.GetComponent<SpriteRenderer>().sprite = building.GetComponent<SpriteRenderer>().sprite;
-        Cursor.visible = false;
+    private readonly Vector2[] neighbourPositions =
+{
+    Vector2.up,
+    Vector2.right,
+    Vector2.down,
+    Vector2.left,
+    Vector2.up + Vector2.right,
+    Vector2.up + Vector2.left,
+    Vector2.down + Vector2.right,
+    Vector2.down + Vector2.left
+};
 
-        buildingToPlace = building;
+    public List<Tile> FindAllTileNeighbors(Vector2 tilePosition)
+    {
+        List<Tile> allNeighbouringTiles = new List<Tile>();
+        foreach (Vector2 neighbourPosition in neighbourPositions)
+        {
+            Vector3 position = tilePosition + neighbourPosition;
+            foreach (Tile tile in tiles)
+            {
+                if (tile.transform.position == position)
+                {
+                    allNeighbouringTiles.Add(tile);
+                }
+            }
+        }
+        return allNeighbouringTiles;
     }
 }
